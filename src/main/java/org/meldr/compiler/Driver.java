f1385841214766658779e8ada5ac16e7ec6f740f
Phil Wigner <moldymoose@protonmail.com>
2025-11-12 10:46:49 -0500

    initial commit with antonio's driver code

---
+++ src/main/java/org/meldr/compiler/Driver.java
@@ -0,0 +1,56 @@
+package org.meldr.compiler;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.PrintWriter;
+//YOU ARE NOT REQUIRED TO MODIFY THIS CLASS
+
+public class Driver 
+{
+    static boolean isSyntaxError = false;
+	
+	public static void main(String[] args) throws Exception {
+
+		// create a CharStream that reads from standard input
+		ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+		// create a lexer that feeds off of input CharStream
+		MeldrLangLexer lexer = new MeldrLangLexer(input);
+
+		// create a buffer of tokens pulled from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		// create a parser that feeds off the tokens buffer
+		MeldrLangParser parser = new MeldrLangParser(tokens);
+        parser.removeErrorListeners();
+		BaseErrorListener errListener = new BaseErrorListener() 
+		{
+
+			@Override
+			public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+			{
+				isSyntaxError = true;
+				System.out.println("Syntax error occurred at Line: " + line + "." + " ( " + msg + " )");
+			}
+		};
+		parser.addErrorListener(errListener);
+		ParseTree tree = parser.scene(); // begin parsing at scene rule
+		if(!isSyntaxError) //throws exceptions if there's syntax errors in our code
+		{
+			// Create a generic parse tree walker that can trigger callbacks
+			ParseTreeWalker walker = new ParseTreeWalker();
+
+			//Class for generating python code neeeded to render user input (extends BaseListener)
+			PythonBuilder stb = new PythonBuilder();
+
+			// Walk the tree created during the parse, trigger callbacks
+			walker.walk(stb, tree);
+			
+			PrintWriter out = new PrintWriter(stb.sceneName + ".py");
+			out.println(stb.printOutput());
+			out.close();	
+		}
+	}
+}
